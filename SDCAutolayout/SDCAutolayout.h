bfd61ea40745956f2405fb2d97bf3a88ce767232
opsbrainbow <ops@brainbowgames.com>
2020-04-07 17:14:49 +0100

    [ADD] inclusion of framework for carthage support

---
+++ SDCAutolayout/SDCAutolayout.h
@@ -0,0 +1,19 @@
+//
+//  SDCAutolayout.h
+//  SDCAutolayout
+//
+//  Created by Matthew Dovey on 07/04/2020.
+//  Copyright © 2020 Scotty Doesn't Code. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDCAutolayout.
+FOUNDATION_EXPORT double SDCAutolayoutVersionNumber;
+
+//! Project version string for SDCAutolayout.
+FOUNDATION_EXPORT const unsigned char SDCAutolayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDCAutolayout/PublicHeader.h>
+
+
