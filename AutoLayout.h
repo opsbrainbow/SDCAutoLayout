4237f20923552cea0f9eb0c7d2bfc0f4c10b2127
opsbrainbow <ops@brainbowgames.com>
2020-04-07 16:11:08 +0100

    [ADD] creation of header file

---
+++ AutoLayout.h
@@ -0,0 +1,9 @@
+//
+//  AutoLayout.h
+//  AutoLayout
+//
+//  Created by Matthew Dovey on 07/04/2020.
+//  Copyright © 2020 Scotty Doesn't Code. All rights reserved.
+//
+
+#import <AutoLayout/UIView+SDCAutoLayout.h>
